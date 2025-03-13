81f183c3c7f042b93deae5ad430cb44c16be9754
Tunde Oretade <tundeiness@yahoo.co.uk>
2025-03-12 22:16:27 -0400

    tweaking to protect secrets

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
+
 }
 
 resource "azurerm_resource_group" "example" {
--- variables.tf
+++ variables.tf
@@ -18,3 +18,23 @@ variable "admin_password" {
   description = "The password for the VM being created."
   default = "PassWord@123"
 }
+
+# variable "subscription" {
+#   description = "The password for the VM being created."
+#   default = "20030103-646c-4afe-8045-99d79bf0d703"
+# }
+
+# variable "client" {
+#   description = "The password for the VM being created."
+#   default = "PassWord@123"
+# }
+
+# variable "secret" {
+#   description = "The password for the VM being created."
+#   default = "PassWord@123"
+# }
+
+# variable "tenant" {
+#   description = "The password for the VM being created."
+#   default = "ec4bf002-4987-420a-a725-bcc2bbd54399"
+# }
